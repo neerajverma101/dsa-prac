4000c939b4ea1e6fcbeabfee26d22ef096e27947
Neeraj Verma <neerajverma101@gmail.com>
2023-02-25 18:25:44 +0530

    palindrome

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
-isPalindrome = function(num) {
-    let revNum=0
-    let remain=0
-    const oriNum=num
-    while(num>0){
-        remain=num%10
-        num=parseInt(num/10)
-        revNum=revNum*10+remain
+const isPalindrome = function (num = 0) {
+    let revNum = 0
+    let remain = 0
+    const oriNum = num
+    while (num > 0) {
+        remain = num % 10
+        num = parseInt(num / 10)
+        revNum = revNum * 10 + remain
     }
-    return oriNum===revNum
+    console.log(`${oriNum} is ${oriNum === revNum ? 'palindrome' : 'not palindrome'}`)
 };
 
 isPalindrome(121)
+isPalindrome(1231)
+isPalindrome(12321)
